0286150b95752662b2f04fa5606ad0bd8fe87606
anngo22 <120738281+anngo22@users.noreply.github.com>
2023-05-18 19:16:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 import pprint
-from functools import wraps
-from typing import Any
 import psycopg
-import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ParseMode
 from aiogram.utils import executor
-from aiogram.types import (
-    ReplyKeyboardRemove,
-    ReplyKeyboardMarkup,
-    KeyboardButton,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-)
 from utils.db import *
 from aiogram.utils.callback_data import CallbackData
-import utils.keyboards as kb
 from config import TOKEN
 
-ADMIN = 805312711
 
 bot = Bot(token=TOKEN)
 
@@ -35,6 +21,10 @@ class Form(StatesGroup):
     name = State()
     age = State()
 
+class Form_money(StatesGroup):
+    money = State()
+
+
 
 @dp.message_handler(commands=["start"])
 async def process_start_command(message: types.Message):
@@ -43,16 +33,74 @@ async def process_start_command(message: types.Message):
 
 @dp.message_handler(commands=["help"])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши\n /library чтобы получить список доступных жанров,\n /register чтобы зарегистрироваться")
+    await message.reply('''Enter\n \n /library to get a list of available genres,\n \n /register to register, \n \n /delete_account to delete an account,\n \n /money to top up your balance, \n \n /check_balance to check your balance''')
 
 
 cb = CallbackData("cb", "handle", "hint", "hint2")
 
+@dp.message_handler(commands=["check_balance"])
+async def process_help_command(message: types.Message):
+    async with await psycopg.AsyncConnection.connect(
+        "dbname=postgres user=postgres host='localhost' password = 'postgres'"
+    ) as aconn:
+        async with aconn.cursor() as acur:
+            await acur.execute(
+                f"""select balance
+                    from e_book_library.user
+                    where user_id = {message.from_user.id};
+                    """
+            )
+            res = await acur.fetchone()
+    if res is None:
+        await message.reply("You are not registered. Please register: /register")
+    else:
+        await message.reply(f"Current balance: {res[0]}")
+    
 
-# @dp.message_handler(commands=["money"])
-# async def process_help_command(message: types.Message):
-#     pass
-#     # SQL for money sending
+@dp.message_handler(commands=["money"])
+async def process_help_command(message: types.Message):
+    async with await psycopg.AsyncConnection.connect(
+        "dbname=postgres user=postgres host='localhost' password = 'postgres'"
+    ) as aconn:
+        async with aconn.cursor() as acur:
+            await acur.execute(
+                f"""select balance
+                    from e_book_library.user
+                    where user_id = {message.from_user.id};
+                    """
+            )
+            res = await acur.fetchone()
+    if res is None:
+        await message.reply("You are not registered. Please register: /register")
+    else:
+        await Form_money.money.set()
+        await message.reply(f"Current balance: {res[0]}. Enter the amount:")
+
+
+@dp.message_handler(state=Form_money.money)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["money"] = message.text
+    async with await psycopg.AsyncConnection.connect(
+        "dbname=postgres user=postgres host='localhost' password = 'postgres'"
+    ) as aconn:
+        async with aconn.cursor() as acur:
+
+            await acur.execute(
+                f"""update e_book_library.user
+                    set balance = balance + {data["money"]}
+                    where user_id = {message.from_user.id};
+                    """
+            )
+            await acur.execute(
+                f"""select balance
+                    from e_book_library.user
+                    where user_id = {message.from_user.id};
+                    """
+            )
+            res = await acur.fetchone()
+    await message.answer(f"Current balance: {res[0]}")
+    await Form_money.next()
 
 
 @dp.message_handler(commands=["library"])
@@ -76,7 +124,8 @@ async def process_library_command(message: types.Message):
         for genre in genres:
             keyboard.add(
                 types.InlineKeyboardButton(
-                    text=genre, callback_data=cb.new(handle="gsh", hint=genre, hint2="")
+                    text=genre, callback_data=cb.new(
+                        handle="gsh", hint=genre, hint2="")
                 )
             )
         await message.answer("Choose your genre", reply_markup=keyboard)
@@ -87,7 +136,6 @@ async def genre_selecter_handler(query: types.CallbackQuery, callback_data: dict
     pprint.pprint(callback_data)
     selected_genre = callback_data["hint"]
     books = await get_books_by_genre(selected_genre)
-
 
     keyboard = types.InlineKeyboardMarkup(resize_keyboard=True)
     for book in books:
@@ -167,13 +215,15 @@ async def money_handler(query: types.CallbackQuery, callback_data: dict):
         reply_markup=keyboard,
     )
 
-@dp.callback_query_handler(cb.filter(handle = 'no'))
+
+@dp.callback_query_handler(cb.filter(handle='no'))
 async def no_handler(query: types.CallbackQuery, callback_data: dict):
     await bot.edit_message_text(
-        'Вы вернулись на главное меню.',
+        'You are back on the main menu.',
         query.from_user.id,
         query.message.message_id,
     )
+
 
 @dp.callback_query_handler(cb.filter(handle="withdrawal_of_money"))
 async def money_handler(query: types.CallbackQuery, callback_data: dict):
@@ -185,18 +235,18 @@ async def money_handler(query: types.CallbackQuery, callback_data: dict):
         ) as aconn:
             async with aconn.cursor() as acur:
                 await acur.execute(
-                f"""update e_book_library.user
+                    f"""update e_book_library.user
                     set balance = balance - {cost}
                     where user_id = {query.from_user.id};"""
-            )
+                )
         await bot.edit_message_text(
-            "approved",
+            "Approved",
             query.from_user.id,
             query.message.message_id,
         )
     except Exception as e:
         await bot.edit_message_text(
-            "insufficient funds",
+            "Insufficient funds. Choose /money to top up your balance.",
             query.from_user.id,
             query.message.message_id,
         )
@@ -239,10 +289,17 @@ async def genre_selecter_handler(query: types.CallbackQuery, callback_data: dict
     )
 
 
-# @dp.message_handler(commands=['delete_account'])
-# async def process_login_command(msg: types.Message):
-#     # sQl for data removal
-#     pass
+@dp.message_handler(commands=['delete_account'])
+async def process_login_command(msg: types.Message):
+    async with await psycopg.AsyncConnection.connect(
+        "dbname=postgres user=postgres host='localhost' password = 'postgres'"
+    ) as aconn:
+        async with aconn.cursor() as acur:
+            await acur.execute(
+                f"""delete from e_book_library.user
+                    where user_id = {msg.from_user.id};"""
+            )
+    await msg.reply("Account deleted.")
 
 
 @dp.message_handler(commands=["register"])
@@ -284,7 +341,7 @@ async def process_age(message: types.Message, state: FSMContext):
                 f"""insert into e_book_library.user (user_id, user_name, age, balance)
                                 values ({message.from_user.id}, '{data['name']}', {int(message.text)}, 0);"""
             )
-    await message.reply("Registration is over")
+    await message.reply("Registration is over. Now you have access to /library")
     await Form.next()
 
 
